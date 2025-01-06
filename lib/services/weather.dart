24898c9b296dad72ce054d692a49710ffda749d4
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-01-07 02:47:55 +0800

    dsad

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,7 +1,7 @@
 import 'package:climaflutter/services/networking.dart';
 import 'package:climaflutter/services/location.dart';
 
-const apiKey = '309b4bb1b5c6c8da17aac8ed00cc6b48';
+
 const openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather';
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 class WeatherModel {
